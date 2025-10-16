99e36bd35199ababa775e51a78c9a48807cc9652
Endergoo <hmboone@email.sc.edu>
2025-10-16 12:53:41 -0400

    swdfwd

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h3>WOWOWOW</h3>
+  </React.StrictMode>
+);
+
